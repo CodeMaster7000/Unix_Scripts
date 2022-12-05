8f6c645390516fe3bff90e06dcbe5dd6747e0e9d
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2022-12-05 21:13:58 +0000

    Create DateTime.sh

---
+++ DateTime.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+Year=`date +%Y`
+Month=`date +%m`
+Day=`date +%d`
+Hour=`date +%H`
+Minute=`date +%M`
+Second=`date +%S`
+echo `date`
+echo "Today's date is: $Day-$Month-$Year"
+echo "Current time: $Hour:$Minute:$Second"
