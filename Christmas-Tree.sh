ee73521371783e2de63660a6a9bd9cd71f1ed52c
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2022-12-30 12:35:41 +0000

    Create Christmas-Tree.sh

---
+++ Christmas-Tree.sh
@@ -0,0 +1,28 @@
+triangle() {
+    a=$1
+    for (( i=0; i<a; i++ )); do
+        for (( j=0; j<=i; j++ )); do
+            echo -n "x"
+        done
+        echo ""
+    done
+}
+christmas_tree() {
+    n=$1
+    for (( i=1; i<=n; i++ )); do
+        triangle "$i"
+    done
+}
+main() {
+    if [ $# -ne 1 ]; then
+        echo "Yuo must provide only one number."
+        exit 1
+    fi
+    re='^[0-9]+$'
+    if ! [[ $1 =~ $re ]]; then
+        echo "$1 si not a positive integer."
+        exit 1
+    fi
+    christmas_tree "$1"
+}
+main "$@"
