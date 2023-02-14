8777faeb57862a41839975aa46b11c185afa14d4
CodeMaster7000 <95772109+CodeMaster7000@users.noreply.github.com>
2023-02-14 13:35:39 +0000

    Update Empty_trash.sh

--- Empty_trash.sh
+++ Empty_trash.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 [ -d ~/.local/share/Trash ] && rm -rf ~/.local/share/Trash/* && echo "Emptied shared trash."
-[ -d ~/.Trash ] && rm -rf ~/.Trash/* && echo "Emptied trash." 
+[ -d ~/.Trash ] && rm -rf ~/.Trash/* && echo "Emptied trash." # For Mac and some other systems.
